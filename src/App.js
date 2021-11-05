52c2deb9c576bf9995efe35bb2a62c2d7f094672
Caleb Hoff <crunchycat1@gmail.com>
2021-11-05 16:58:21 -0500

    Basic Main Interface

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App text-center">
+      <div className="cover-container d-flex h-100 p-3 mx-auto flex-column">
+        <header className="masthead mb-auto">
+          <div className="inner">
+            <h3 className="masthead-brand">codenames</h3>
+            <nav className="nav nav-masthead justify-content-center"><a className="nav-link active" href="#">play</a><a className="nav-link" href="#">how to play</a><a className="nav-link" href="#">changes</a></nav>
+          </div>
+        </header>
+        <main className="inner cover" role="main">
+          <h1 className="cover-heading">start or join a session</h1>
+          <p className="lead">codenames is a game of guessing words related to the hints given by your spymaster</p>
+          <p className="lead"><a className="btn btn-lg btn-secondary" href="#">host game</a></p>
+          <p className="lead"><a className="btn btn-lg btn-secondary" href="#">join game</a></p>
+        </main>
+        <footer className="mastfoot mt-auto">
+          <div className="inner">
+            <p>Created by <a href="http://www.calebhoff.com/">Caleb Hoff</a></p>
+          </div>
+        </footer>
+      </div>
     </div>
   );
 }
