08745bd607316ff209727c0ce3dcc9b793e0b1d9
Caleb Hoff <crunchycat1@gmail.com>
2021-11-06 15:47:49 -0500

    Colors

--- src/components/Game/Game.tsx
+++ src/components/Game/Game.tsx
@@ -3,24 +3,58 @@ import { Link, useParams } from "react-router-dom";
 import seedrandom from 'seedrandom';
 import './Game.css';
 
-let words = ['loading'];
+let cards = [{word: 'loading', color: 'dark', turned: false}];
 let refreshWords = true;
-let rng;
 
 const Game = () => {
+  const [_, forceUpdate] = useReducer(x => x + 1, 0);
   let { seed } = useParams();
-  rng = seedrandom(seed);
+  const rng = seedrandom(seed);
+
+  const Board = () => {
+    const runCallback = (f) => f();
+    return (
+      <div className="board">
+        {runCallback(() => {
+          const rows = [];
+          cards.forEach((card, index) => {
+            rows.push(
+              <button className={`card m-2 card-${card.turned ? card.color : 'white'}`} key={card.word + index} onClick={flipCard(index)}>
+                <div className="card-body">{card.word}</div>
+              </button>
+            );
+          });
+          return rows;
+        })}
+      </div>
+    )
+  }
   
-  const [ignored, forceUpdate] = useReducer(x => x + 1, 0);
-  if (refreshWords) loadWords().then(() => forceUpdate());
+  const flipCard = (index) => () => {
+    cards[index].turned = true;
+    forceUpdate();
+  }
+  
+  const loadWords = async () => {
+    refreshWords = false;
+    const words = (await (await fetch('/nouns.txt')).text()).split('\n').sort(() => .5 - rng());
+    cards = words.slice(0, 9).map(word => ({word, color: 'blue', turned: false}))
+    .concat(words.slice(9, 17).map(word => ({word, color: 'red', turned: false})),
+            words.slice(17, 24).map(word => ({word, color: 'yellow', turned: false})),
+            words.slice(24, 25).map(word => ({word, color: 'black', turned: false})));
+    cards.sort(() => rng() - 0.5);
+    forceUpdate();
+  }
+  
+  if (refreshWords) loadWords(); // Asynchronously Load Words
 
   return (
   <div className="Game">
     <div className="text-center">
-      <div className="cover-container d-flex h-100 p-3 mx-auto flex-column">
+      <div className="cover-container d-flex h-100 mx-auto flex-column">
         <header className="masthead mb-auto">
           <div className="inner">
-            <h3 className="masthead-brand">codenames</h3>
+          <Link to="/ "><h3 className="masthead-brand">codenames</h3></Link>
             <nav className="nav nav-masthead justify-content-center">
               <Link className="nav-link active" to="/local">game</Link>
               <Link className="nav-link" to="/stats">stats</Link>
@@ -28,9 +62,9 @@ const Game = () => {
             </nav>
           </div>
         </header>
-        <main className="inner cover p-3" role="main">
-          <h1>Game Board</h1>
-          <p className="lead">blue team starts</p>
+        <main className="inner cover" role="main">
+          <h1 className="mt-2 mb-0">game board</h1>
+          <p className="lead mt-0 mb-2">blue team starts</p>
         </main>
       </div>
     </div>
@@ -38,29 +72,5 @@ const Game = () => {
   </div>
   );
 };
-
-function Board() {
-  const runCallback = (f) => f();
-  return (
-    <div className="board">
-      {runCallback(() => {
-        const rows = [];
-        words.forEach((word) => {
-          rows.push(
-            <div className="card" key={word}>
-              <div className="card-body">{word}</div>
-            </div>
-          );
-        });
-        return rows;
-      })}
-    </div>
-  )
-}
-
-const loadWords = async () => {
-  refreshWords = false;
-  words = (await (await fetch('/nouns.txt')).text()).split('\n').sort(() => .5 - rng()).slice(0, 25);
-}
 
 export default Game;
