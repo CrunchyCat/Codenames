b8acba7dac18c209497b39076b59b14dab63674a
Caleb Hoff <crunchycat1@gmail.com>
2021-11-06 16:30:00 -0500

    turns & gameover

--- src/components/Game/Game.tsx
+++ src/components/Game/Game.tsx
@@ -5,6 +5,9 @@ import './Game.css';
 
 let cards = [{word: 'loading', color: 'dark', turned: false}];
 let refreshWords = true;
+let textInfo = 'blue team starts';
+let turn = 'blue';
+let gameover = false;
 
 const Game = () => {
   const [_, forceUpdate] = useReducer(x => x + 1, 0);
@@ -19,7 +22,7 @@ const Game = () => {
           const rows = [];
           cards.forEach((card, index) => {
             rows.push(
-              <button className={`card m-2 card-${card.turned ? card.color : 'white'}`} key={card.word + index} onClick={flipCard(index)}>
+              <button className={`card m-2 card-${card.turned ? card.color : 'white'}`} key={card.word + index} onClick={flipCard(card)}>
                 <div className="card-body">{card.word}</div>
               </button>
             );
@@ -30,8 +33,18 @@ const Game = () => {
     )
   }
   
-  const flipCard = (index) => () => {
-    cards[index].turned = true;
+  const flipCard = (card) => () => {
+    if (card.turned || gameover) {
+      return;
+    }
+    card.turned = true;
+    if (card.color === 'black') {
+      gameover = true;
+      textInfo = `gameover, ${turn} team wins`
+    } else if (card.color !== turn) {
+      turn = turn === 'blue' ? 'red' : 'blue';
+      textInfo = turn === 'blue' ? 'turn: blue' : 'turn: red';
+    }
     forceUpdate();
   }
   
@@ -49,27 +62,28 @@ const Game = () => {
   if (refreshWords) loadWords(); // Asynchronously Load Words
 
   return (
-  <div className="Game">
-    <div className="text-center">
-      <div className="cover-container d-flex h-100 mx-auto flex-column">
-        <header className="masthead mb-auto">
-          <div className="inner">
-          <Link to="/ "><h3 className="masthead-brand">codenames</h3></Link>
-            <nav className="nav nav-masthead justify-content-center">
-              <Link className="nav-link active" to="/local">game</Link>
-              <Link className="nav-link" to="/stats">stats</Link>
-              <Link className="nav-link" to="/settings">settings</Link>
-            </nav>
+    <div className="Game">
+        <div className="text-center">
+          <div className="cover-container d-flex h-100 mx-auto flex-column">
+            <header className="masthead mb-auto">
+              <div className="inner">
+              <Link to="/ "><h3 className="masthead-brand">codenames</h3></Link>
+                <nav className="nav nav-masthead justify-content-center">
+                  <Link className="nav-link active" to={`/local/${seed}`}>game</Link>
+                  {/*TODO: Implement Codemasters & Settings Sections & Remove Disables */}
+                  <Link className="nav-link disabled" to={`/local/${seed}/codemasters`}>codemasters</Link>
+                  <Link className="nav-link disabled" to={`/local/${seed}/settings`}>settings</Link>
+                </nav>
+              </div>
+            </header>
+            <main className="inner cover" role="main">
+              <h1 className="mt-2 mb-0">game board</h1>
+              <p className="lead mt-0 mb-2">{textInfo}</p>
+            </main>
           </div>
-        </header>
-        <main className="inner cover" role="main">
-          <h1 className="mt-2 mb-0">game board</h1>
-          <p className="lead mt-0 mb-2">blue team starts</p>
-        </main>
+        </div>
+        <Board />
       </div>
-    </div>
-    <Board />
-  </div>
   );
 };
 
